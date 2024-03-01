9db72b0cfd79b4602f7208d59a911eeda37181be
Diptayan Jash <dj2037@srmist.edu.in>
2024-03-02 01:10:10 +0530

    Fix certificate link handling in PastEvents component

--- components/Events/PastEvents/PastEvents.jsx
+++ components/Events/PastEvents/PastEvents.jsx
@@ -12,26 +12,32 @@ const dmsans = DM_Sans({
 });
 
 const PastEvents = ({
+    poster,
     title,
     certificateLink,
 }) => {
 
     const GetCertificate = () => {
-        window.open(certificateLink, '_blank');
+        if (!certificateLink.startsWith('http://') && !certificateLink.startsWith('https://')) {
+            certificateLink = `https://${certificateLink}`;
+        }
+        window.location.href = certificateLink;
     };
+
 
     return (
         <div className={`block outline-2 outline-white p-4 rounded-lg max-w-[100%] ${dmsans.className}`}>
             <div className="relative">
                 <img
                     alt=""
-                    src="https://s3-alpha-sig.figma.com/img/ea69/5937/2d0ba426506ee31eb9f73d09f81e8c1e?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FC4aflQ4xaOm8GdnX2JdjhDPY24ywS5cIxZCI4Z7f9Qyz~iWgry9AFIrNF5WUmoT06mZJ16Nk6W866C9oHzApXHzlERE~35qkrvxrp5xzBjr1ESff78WqxcTblOINdu2~2sGtN2XbdplmMVDX79lfzaNGStFhkcN2UnklaYQKK-5zhqIRopGuKEltLlcuRXi4WxT7JLFSXMdoYChOk2ErE6SIzfTBMLzKjLaYOl3nyQyN-1zMeEcSuMTIkyO20rUaqSS1SGmbIPi9Nw0uaRtT~prdDQK4qKljJnqRBoliYic9e~8o8NTqY1DbNc7fnkbMAC3s8~NPlQi5R-utpfrtQ__"
+                    src={poster}
                     className="h-full w-full object-cover rounded-lg border-gradient filter drop-shadow-glow"
                 />
             </div>
 
             <div className="flex items-center mt-4">
                 <h3 className="text-lg font-bold text-white sm:text-xl py-2 px-4 pl-0">{title}</h3>
+
                 <button
                     onClick={GetCertificate}
                     className="ml-auto filter bg-bright_green hover:bg-green-700 text-black font-bold rounded h-9 w-40"
