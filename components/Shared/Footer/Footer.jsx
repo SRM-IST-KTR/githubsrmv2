ae7c4d4e78221585ac97bb544f37a284799af2eb
Anushka Priyadarshi <anushka@Anushkas-MacBook-Air-2.local>
2024-03-14 16:12:13 +0530

    Make Footer

--- components/Shared/Footer/Footer.jsx
+++ components/Shared/Footer/Footer.jsx
@@ -2,9 +2,28 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>This is the footer</p>
+    <>
+    <footer className="grid justify-items-center sm:flex sm:justify-between sm:space-y-0 space-y-10 p-20 sm:p-36 ">
+      <div>
+      <img className="w-30 h-12 sm:w-56 sm:h-16" src="Logo.png"/>
+      </div>
+      
+
+      <div className="sm:space-y-2">
+        <h className="flex sm:justify-start mb-3">Follow us on</h>
+        <div className="flex space-x-3">
+          <a>
+          <img className="h-6 w-6 md:h-10 md:w-10" src="linkden.png"/>
+          </a>
+          <a><img className="h-6 w-6 md:h-10 md:w-10" src="instagram_logo.png"/></a>
+          <a><img className="h-6 w-6 md:h-10 md:w-10" src="x_logo.png"/></a>
+          <a><img className="h-6 w-6 md:h-10 md:w-10" src="github_logo.png"/></a>
+        </div>
+      </div>
+
     </footer>
+    <p className="flex justify-center mb-10">Created by GCSRM Team 🐐</p>
+    </>
   );
 };
 
