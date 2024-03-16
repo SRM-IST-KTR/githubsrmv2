c2c3bb313d779aaa94101918c1f28549089b2f84
Lohit Kolluri <lohitkolluri@gmail.com>
2024-03-16 09:44:23 +0530

    💄 Add AboutUs Section

---
+++ components/Home/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <section className="about-us">
+        <h2 className="about-us-title">About Us</h2>
+        <p className="about-us-description">
+          GitHub Community SRM is the official student-led community affiliated
+          with GitHub, spearheading the open-source revolution at SRMIST. With
+          the vision of revolutionising the technical world, we constantly
+          strive to impart the best knowledge on emerging technologies. We have
+          reached 1k+ followers on Instagram, and nearly 500 followers on
+          LinkedIn within 6 months.
+          <br />
+          <br />
+          At GitHub Community SRM, we believe in a friendly work environment,
+          developing and nurturing students at an apt pace. This qualifies us as
+          the first community in SRMIST, Kattankulathur. So what are you waiting
+          for, go ahead and register on the link in bio and become a part of
+          this vibrant community!
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default AboutUs;
