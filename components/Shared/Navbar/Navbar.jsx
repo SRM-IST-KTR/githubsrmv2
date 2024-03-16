a1fc81ef12f80db01b2abb733eced042cb5208b1
Anushka Das <iamanushkadas@gmail.com>
2024-03-16 22:45:31 +0530

    navbar

--- components/Shared/Navbar/Navbar.jsx
+++ components/Shared/Navbar/Navbar.jsx
@@ -2,21 +2,19 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
+    <nav className="bg-transparent top-0 pt-12 pr-24">
+      <div className="flex items-center">
+        <div className="flex items-center">
+          <img src=".\public\logo.png" alt="Logo" className="h-8 w-auto pl-24" />
+        </div>
+        <div className="ml-auto">
+          <a href="/" className="text-white ml-4">Home</a>
+          <a href="team" className="text-white ml-4">Our Team</a>
+          <a href="index" className="text-white ml-4">Events</a>
+          <a href="about" className="text-white ml-4">Our Story</a>
+          <a href="contact" className="text-white ml-4">Contact</a>
+        </div>
+      </div>
     </nav>
   );
 };
