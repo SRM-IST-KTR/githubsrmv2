d9a2595302e3b1a6e619f509fb949ef3764f3b65
Akshat Agarwal <akshataga20@gmail.com>
2024-03-17 01:10:58 +0530

    Background glow needs to be fixed. Our domians flexbox needs to be added.

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,75 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const Index = () => {
+    return (
+        <>
+            <div className="bg-transparent">
+                <div className="0px mx-auto p-24 font-family:Inter">
+                    <h1 className="text-4xl font-bold">GitHub</h1>
+                    <h2 className="text-4xl font-bold">Community SRM</h2>
+                    <p className="mt-4 text-lg justify-content: space-evenly">
+                        The Official student-led community affiliated with
+                        GitHub
+                    </p>
+                    <p className="text-lg justify-content: space-evenly">
+                        spearheading the open-source revolution at SRMIST
+                    </p>
+                    <p
+                        className="mt-4 text-lg font-weight:700 text-black text-center  bg-green-500 justify-center" /*This is horrendus btw */
+                        style={{
+                            width: "187px",
+                            height: "44px",
+                            borderRadius: "30px",
+                        }}
+                    >
+                        Join Us
+                    </p>
+                </div>
+            </div>
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <section class="flex-auto px-20 m-10 hover:ring-green-600">
+                <div className="px-20  py-10 bg-transparent shadow-lg shadow-green-400 rounded-lg  ">
+                    <h2 class="text-4xl font-bold">About Us</h2>
+                    <p class="text-lg text-white  mt-5">
+                        GitHub Community SRM is the official student-led
+                        community affiliated with GitHub, spearheading the
+                        open-source revolution at SRMIST. With the vision of
+                        revolutionising the technical world, we constantly
+                        strive to impart the best knowledge on emerging
+                        technologies. We have reached 1k+ followers on
+                        Instagram, and nearly 500 followers on LinkedIn within 6
+                        months
+                    </p>
+                    <p class="text-lg text-white y mt-5">
+                        At GitHub Community SRM, we believe in a friendly work
+                        environment, developing and nurturing students at an apt
+                        pace. This qualifies us as the first community in
+                        SRMIST, Kattankulathur. So what are you waiting for, go
+                        ahead and register on the link in bio and become a part
+                        of this vibrant community!
+                    </p>
+                </div>
+            </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <section class="mt-10 self-center ">
+                <h2 class="text-4xl font-bold text-center p-10">Our Domains</h2>
+                <div class=" container flex self-auto justify-center place-content-center">
+                    <div class="box-border h-30 w-30 rounded-md px-4 py-4 transition ease-in-out delay-150 bg-transparent hover:-translate-y-1 hover:scale-110 hover:shadow-lime-900 duration-300">
+                        <h3>Technical</h3>
+                    </div>
+                    <div class="shadow-md rounded-md px-4 py-4 transition ease-in-out delay-150 bg-transparent hover:-translate-y-1 hover:scale-110 hover:shadow-lime-900 duration-300">
+                        <h3>Creatives</h3>
+                    </div>
+                    <div class="shadow-md rounded-md px-4 py-4 transition ease-in-out delay-150 bg-transparent hover:-translate-y-1 hover:scale-110 hover:shadow-lime-900 duration-300">
+                        <h3>Corporate</h3>
+                    </div>
+                    <div class="shadow-md rounded-md px-4 py-4 transition ease-in-out delay-150 bg-transparent hover:-translate-y-1 hover:scale-110 hover:shadow-lime-900 duration-300">
+                        <h3>Content</h3>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+export default Index;
