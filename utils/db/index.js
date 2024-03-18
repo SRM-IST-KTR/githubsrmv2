79439b7bf12f94a71855ed6f4cda4523afca5124
Diptayan Jash <dj2037@srmist.edu.in>
2024-03-18 20:38:55 +0530

    Add MongoDB connection utility

---
+++ utils/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const { NEXT_PUBLIC_MONGO_URI, NEXT_PUBLIC_DB_NAME } = process.env;
+
+const DBInstance = async () => {
+    try {
+        await mongoose.connect(NEXT_PUBLIC_MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: NEXT_PUBLIC_DB_NAME
+        });
+
+        // console.log(`✅ Connected to MongoDB: ${NEXT_PUBLIC_DB_NAME}`);
+    } catch (err) {
+        // console.error("❌ Could not connect to MongoDB\n", err.message);
+        throw err;
+    }
+};
+
+export default DBInstance;
