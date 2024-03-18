b76748003fdf8d6b9b361153d74e2f0964294c09
Diptayan Jash <dj2037@srmist.edu.in>
2024-03-19 01:52:25 +0530

    Refactor Hero component to include key and isActive props

--- components/Events/LiveEvents/Hero.jsx
+++ components/Events/LiveEvents/Hero.jsx
@@ -10,8 +10,10 @@ const dmsans = DM_Sans({
 });
 
 const Hero = ({
-    poster,
+    key,
+    isActive,
     title,
+    poster,
     date,
     location,
     timer,
@@ -25,17 +27,16 @@ const Hero = ({
         window.location.href = certificateLink;
     };
 
-    if (title) {
+    if (isActive) {
         return (
             < div className={`w-screen p-4 mx-auto ${dmsans.className}`}>
                 <div className="flex flex-col md:flex-row xl:mx-48 gap-16 justify-center">
                     <div className="relative group">
                         <div className="rounded-lg">
-                            <Image //USE img TAG IF LINK IS PROVIDED
-                                // src="https://s3-alpha-sig.figma.com/img/ea69/5937/2d0ba426506ee31eb9f73d09f81e8c1e?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FC4aflQ4xaOm8GdnX2JdjhDPY24ywS5cIxZCI4Z7f9Qyz~iWgry9AFIrNF5WUmoT06mZJ16Nk6W866C9oHzApXHzlERE~35qkrvxrp5xzBjr1ESff78WqxcTblOINdu2~2sGtN2XbdplmMVDX79lfzaNGStFhkcN2UnklaYQKK-5zhqIRopGuKEltLlcuRXi4WxT7JLFSXMdoYChOk2ErE6SIzfTBMLzKjLaYOl3nyQyN-1zMeEcSuMTIkyO20rUaqSS1SGmbIPi9Nw0uaRtT~prdDQK4qKljJnqRBoliYic9e~8o8NTqY1DbNc7fnkbMAC3s8~NPlQi5R-utpfrtQ__"
+                            <img //USE img TAG IF LINK IS PROVIDED
                                 src={poster}
                                 alt="Upcoming Event"
-                                className="rounded-lg lg:w-[682px]"
+                                className="rounded-lg lg:w-[682px] lg:h-[526px]"
                             />
                         </div>
                     </div>
