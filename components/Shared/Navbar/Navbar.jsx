c369f0108dbbc8d8650b2b54cfef0f6a447cf1a2
Anushka Das <iamanushkadas@gmail.com>
2024-03-23 01:37:32 +0530

    navbar

--- components/Shared/Navbar/Navbar.jsx
+++ components/Shared/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 const Navbar = () => {
+  const [currentPath, setCurrentPath] = useState("");
+  useEffect(() => {
+    setCurrentPath(window.location.pathname);
+  }, []);
+  const isCurrentPath = (url) => {
+    return currentPath === url;
+  };
+
   return (
     <nav className="bg-transparent top-0 pt-12 pr-24">
       <div className="flex items-center">
         <div className="flex items-center">
-          <img src=".\public\logo.png" alt="Logo" className="h-8 w-auto pl-24" />
+          <img src="logo.png" className="h-53 w-151 pl-24" alt="Logo" />
         </div>
         <div className="ml-auto">
-          <a href="/" className="text-white ml-4">Home</a>
-          <a href="team" className="text-white ml-4">Our Team</a>
-          <a href="index" className="text-white ml-4">Events</a>
-          <a href="about" className="text-white ml-4">Our Story</a>
-          <a href="contact" className="text-white ml-4">Contact</a>
+          <a href="/" className={`ml-4 ${isCurrentPath('/') ? 'text-bright_green' : ''}`}>Home</a>
+          <a href="/team" className={`ml-4 ${isCurrentPath('/team') ? 'text-bright_green' : ''}`}>Our Team</a>
+          <a href="/events" className={`ml-4 ${isCurrentPath('/events') ? 'text-bright_green' : ''}`}>Events</a>
+          <a href="/about" className={`ml-4 ${isCurrentPath('/about') ? 'text-bright_green' : ''}`}>Our Story</a>
+          <a href="/contact" className={`ml-4 ${isCurrentPath('/contact') ? 'text-bright_green' : ''}`}>Contact Us</a>
         </div>
       </div>
     </nav>
@@ -20,3 +27,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
