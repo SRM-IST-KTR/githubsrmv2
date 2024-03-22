c6647ec757788b477e7cf498050675177a16fbc3
Anushka Priyadarshi <anushka@Anushkas-MacBook-Air-2.local>
2024-03-22 11:35:05 +0530

    fiz sizing, add hyperlinks

--- components/Shared/Footer/Footer.jsx
+++ components/Shared/Footer/Footer.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import {FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -10,15 +14,15 @@ const Footer = () => {
       
 
       <div className="sm:space-y-2">
-        <h className="flex sm:justify-start mb-3">Follow us on</h>
+        <h className="flex sm:justify-start mb-3">Follow us on </h>
+
         <div className="flex space-x-3">
-          <a>
-          <img className="h-6 w-6 md:h-10 md:w-10" src="linkden.png"/>
-          </a>
-          <a><img className="h-6 w-6 md:h-10 md:w-10" src="instagram_logo.png"/></a>
-          <a><img className="h-6 w-6 md:h-10 md:w-10" src="x_logo.png"/></a>
-          <a><img className="h-6 w-6 md:h-10 md:w-10" src="github_logo.png"/></a>
+          <a href="https://www.linkedin.com/company/githubsrm/mycompany/"><FaLinkedin className="h-7 w-7 md:h-10 md:w-10" /></a>
+          <a href="https://www.instagram.com/githubsrm/?hl=en"><FaInstagram className="h-7 w-7 md:h-10 md:w-10" /></a>
+          <a href="https://twitter.com/GithubSrm"><FaXTwitter className="h-7 w-7 md:h-10 md:w-10" /></a>
+          <a href="https://github.com/SRM-IST-KTR"><FaGithub className="h-7 w-7 md:h-10 md:w-10" /></a>
         </div>
+        
       </div>
 
     </footer>
