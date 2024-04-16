4ee56a408607b14b96308d109fab6662d56a7d74
GauriChapra <gaurichapra12@gmail.com>
2024-04-16 19:43:20 +0530

    updated body section

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Shared/Navbar/Navbar";
 import Footer from "@/components/Shared/Footer/Footer";
+import BodyStory from "@/components/OurStory/BodyStory";
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <BodyStory />
       <Footer />
     </>
   );
