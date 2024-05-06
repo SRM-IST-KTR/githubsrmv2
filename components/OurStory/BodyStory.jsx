91c0fa1a9d5474fbf8fe50715c2422384f0be34f
PYTRanger <1406utkarsh@gmail.com>
2024-05-06 19:30:06 +0530

    fixed horizontal overflow

--- components/OurStory/BodyStory.jsx
+++ components/OurStory/BodyStory.jsx
@@ -1,22 +1,13 @@
-
-import { DM_Sans } from 'next/font/google';
-
-const dmsans = DM_Sans({
-    subsets: ['latin'],
-    display: 'swap',
-});
-
 const BodyStory = ({
     title,
 }) => {
     return (
-        <div className="bg-black p-4 h-screen w-screen">
+        <div className="bg-black p-4 h-screen w-screen" style={{maxWidth: '100%', overflowX: 'hidden'}}>
          <div className="flex items-center justify-center mt-[10vh] h-fit sm:h-[70vh] w-[90vw] sm:w-[70vw] bg-transparent rounded-lg border drop-shadow-[0_0_15px_#82F6A2] hover:bg-gradient-to-b from-[#00E43D] via-[#000000] to-[#00E43D] border-transparent m-auto">
                 <div className="bg-black justify-center h-auto sm:h-[69vh] w-full sm:w-[69.25vw] rounded-lg">
                     <h3 className="text-2xl text-center font-bold text-[#00E43D] pt-4">Our Story</h3>
                     <p className='text-white p-4 text-sm'>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.              </p>
                     <p className='text-white p-4 text-sm'>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
                     </p>
