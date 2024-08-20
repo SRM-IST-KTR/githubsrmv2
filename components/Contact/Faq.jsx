9247fc68ab8cf1894ce67f4cefb76083bce2ebac
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-08-20 16:10:28 +0530

    chore: Update FAQ component styling and font usage

--- components/Contact/Faq.jsx
+++ components/Contact/Faq.jsx
@@ -13,35 +13,33 @@ const Faq = () => {
     };
 
     return (
-        <div className="my-20 py-10  text-white">
+        <div className="my-20 py-10  text-white font-dmSans">
             <div className="overflow-hidden">
-                <h1 className="text-4xl font-roboto font-bold text-center mb-16">
+                <h1 className="text-4xl font-poppins font-bold text-center mb-16">
                     Frequently Asked Questions
                 </h1>
-                <div className="flex flex-col items-center space-y-6">
+                <div className="flex flex-col items-center space-y-4 w-">
                     {faqData.map((item, index) => (
                         <div
                             key={index}
-                            className="border-2 rounded-2xl mx-auto transition-transform ease-in-out duration-300 transform hover:shadow-lg hover:border-bright_green"
-                            style={{ borderColor: "#374151", width: "60%" }}
+                            className=" font-dmSans border-2 rounded-2xl mx-auto transition-transform ease-in-out duration-300 transform hover:shadow-lg hover:border-bright_green w-4/5 md:w-3/5"
+                            style={{ borderColor: "#374151" }}
                         >
                             <div
                                 className="cursor-pointer"
                                 onClick={() => toggleDropdown(index)}
                             >
                                 <div
-                                    className={`flex items-center justify-between p-4 pl-6 transition ease-in-out duration-700 ${
-                                        openAnswer === index
-                                            ? "bg-bright_green text-black rounded-t-2xl"
-                                            : ""
-                                    }`}
+                                    className={`flex items-center justify-between p-4 pl-6 transition ease-in-out duration-700 ${openAnswer === index
+                                        ? "bg-bright_green text-black rounded-t-2xl"
+                                        : ""
+                                        }`}
                                 >
                                     <span
-                                        className={`text-lg ${
-                                            openAnswer === index
-                                                ? "fira-code font-bold"
-                                                : ""
-                                        }`}
+                                        className={`text-lg ${openAnswer === index
+                                            ? "fira-code font-bold"
+                                            : ""
+                                            }`}
                                     >
                                         {item.question}
                                     </span>
