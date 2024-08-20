e036d2f859255ba56bf900ddad41107d3541bbb4
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-08-20 16:11:22 +0530

    chore: Update Hero component styling and font usage in OurStory page

--- components/OurStory/Hero.jsx
+++ components/OurStory/Hero.jsx
@@ -4,7 +4,7 @@ import aboutUsHero from "@/public/teamPic2.jpg";
 const Hero = () => {
     return (
         <section
-            className="mt-0 top-0 overflow-x-hidden h-[60vh]"
+            className="mt-0 top-0 overflow-x-hidden h-[100vh] bg-opacity-50 bg-center bg-no-repeat bg-cover relative"
             style={{
                 backgroundImage: `url(${aboutUsHero.src})`,
                 backgroundSize: "cover",
@@ -12,14 +12,14 @@ const Hero = () => {
                 position: "relative",
                 backgroundAttachment: "fixed", //fix the background image while scrolling to look good
             }}>
-            <div className=' py-8 md:py-16 lg:py-16'>
+            <div className='py-64 md:py-16 lg:py-64 font-poppins'>
                 <div className="flex justify-start items-center relative z-10">
                     <div className="text-left">
-                        <h1 className="lg:text-7xl text-4xl font-bold mt-10 xl:mt-20 md:mt-20 px-8 lg:px-60 text-bright_green">
+                        <h1 className="lg:text-7xl text-4xl font-bold font-poppins mt-10 xl:mt-20 md:mt-20 px-8 lg:px-60 text-bright_green">
                             Our Story
                         </h1>
                         <br />
-                        <div className="lg:text-7xl text-2xl font-semibold px-8 lg:px-60 text-white">
+                        <div className="lg:text-7xl text-2xl font-semibold font-poppins px-8 lg:px-60 text-white">
                             Github Community SRM
                         </div>
                         <br />
