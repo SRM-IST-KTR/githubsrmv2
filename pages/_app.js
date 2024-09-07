ec31ad8836624a1e568d7dfc504a5b537faff771
saranshsinhaa <saranshsinha084@gmail.com>
2024-09-08 02:30:35 +0530

    ♿️ Add favicon and title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Shared/Navbar/Navbar";
 import Footer from "@/components/Shared/Footer/Footer";
+import Head from "next/head";
+
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <title>GitHub Community SRM</title>
+                <meta
+                    name="description"
+                    content="The Official student-led community affiliated with GitHub, Spearheading the open-source revolution at SRMIST"
+                />
+            </Head>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
