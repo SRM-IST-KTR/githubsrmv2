2c436fb6dd2d4befa5eb9ea54af6f6cdd2c15ac7
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-09-19 17:49:16 +0530

    SEO

--- pages/recruitment.js
+++ pages/recruitment.js
@@ -5,6 +5,7 @@ import recruitmentpos from "@/public/recruitmentpos.jpg";
 import DomainCard from '@/components/Recruitments/DomainCard';
 import Hero from '@/components/Recruitments/Hero';
 import { MdBuild, MdBusiness, MdBrush } from "react-icons/md"; // Icons for the domains
+import Head from "next/head";
 function recruitment() {
     const domains = [
         {
@@ -30,7 +31,56 @@ function recruitment() {
         }
     ];
     return (
-        <>
+        <div className='bg-bg_black'>
+            <Head>
+                <title>Recruitments | GitHub Community SRM</title>
+                <meta
+                    name="description"
+                    content="Learn about GitHub Community SRM, the official student-led group at SRMIST, fostering a collaborative open-source culture."
+                />
+                <meta
+                    name="keywords"
+                    content="GitHub, SRMIST, Open Source, Coding, GitHub Community SRM, Development, Collaboration, recruitment, join, community, apply"
+                />
+                <meta name="author" content="GitHub Community SRM" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="canonical" href="https://githubsrmist.tech/recruitment" />
+
+                <meta name="twitter:card" content="/public/x_logo.png" />
+                <meta
+                    name="twitter:title"
+                    content="Recruitments - GitHub Community SRM"
+                />
+                <meta
+                    name="twitter:description"
+                    content="Discover the story behind GitHub Community SRM, a hub for collaborative open-source projects at SRMIST."
+                />
+                <meta name="twitter:image" content="/public/x_logo.png" />
+                <meta name="twitter:site" content="@GithubSrm" />
+                <meta
+                    name="twitter:url"
+                    content="https://githubsrmist.tech/recruitment"
+                />
+
+                <meta
+                    property="og:title"
+                    content="About Us - GitHub Community SRM"
+                />
+                <meta
+                    property="og:description"
+                    content="GitHub Community SRM is a space for collaboration and open-source innovation at SRMIST. Learn more about our journey."
+                />
+                <meta
+                    property="og:url"
+                    content="https://githubsrmist.tech/recruitment"
+                />
+                <meta property="og:image" content="/public/logo.png" />
+                <meta property="og:type" content="website" />
+                <meta property="og:site_name" content="GitHub Community SRM" />
+            </Head>
             <section
                 className="relative mt-0 top-0 overflow-x-hidden"
                 style={{
@@ -64,7 +114,7 @@ function recruitment() {
 
             <FormSub />
 
-        </>
+        </div>
     );
 }
 
