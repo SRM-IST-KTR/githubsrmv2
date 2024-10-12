2c4f0920fc59515ec8cd96e3a86c77d3f8f383e3
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-10-13 01:52:57 +0530

    updated New team structure. lets gooooo

--- pages/team.js
+++ pages/team.js
@@ -3,6 +3,7 @@ import Hero from "@/components/Team/Hero";
 import ProfileCard from "@/components/Team/profileCard";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Head from "next/head";
+import { set } from "mongoose";
 
 const ProfileSkeleton = () => {
     return (
@@ -14,8 +15,13 @@ const ProfileSkeleton = () => {
 
 const Teams = () => {
     const [fetched, setFetched] = useState(false);
+    const [convenor, setConvenor] = useState(null);
+    const [president, setPresident] = useState(null);
+    const [vp, setVp] = useState(null);
+
     const [admins, setAdmins] = useState([]);
     const [leads, setLeads] = useState([]);
+    const [associates, setAssociates] = useState([]);
     const [members, setMembers] = useState([]);
 
     const [domain, setDomain] = useState("Technical");
@@ -29,18 +35,35 @@ const Teams = () => {
                 }
                 const data = await response.json();
 
+                const convenorData = data.data.find(
+                    (item) => item.position === "Convenor"
+                );
+                const presidentData = data.data.find(
+                    (item) => item.position === "President"
+                );
+                const vpData = data.data.find(
+                    (item) => item.position === "vicePresident"
+                );
+
                 const adminsData = data.data.filter(
                     (item) => item.position === "Admin"
                 );
                 const leadsData = data.data.filter(
                     (item) => item.position === "Lead"
                 );
+                const associatesData = data.data.filter(
+                    (item) => item.position === "Associate"
+                );
                 const membersData = data.data.filter(
                     (item) => item.position === "Member"
                 );
 
+                setConvenor(convenorData);
+                setPresident(presidentData);
+                setVp(vpData);
                 setAdmins(adminsData);
                 setLeads(leadsData);
+                setAssociates(associatesData);
                 setMembers(membersData);
                 setFetched(true);
             } catch (error) {
@@ -59,6 +82,7 @@ const Teams = () => {
 
     const filteredAdmins = admins.filter((admin) => admin.domain === domain);
     const filteredLeads = leads.filter((lead) => lead.domain === domain);
+    const filteredAssociates = associates.filter((associates) => associates.domain === domain);
     const filteredMembers = members.filter(
         (member) => member.domain === domain
     );
@@ -76,12 +100,12 @@ const Teams = () => {
                     content={`${admins
                         .map((admin) => admin.name)
                         .join(", ")}, ${leads
-                        .map((lead) => lead.name)
-                        .join(", ")}, ${members
-                        .map((member) => member.name)
-                        .join(
-                            ", "
-                        )}, GitHub Community SRM team, Technical domain, Corporate domain, Creatives domain, Content domain, GitHub, LinkedIn, Instagram`}
+                            .map((lead) => lead.name)
+                            .join(", ")}, ${members
+                                .map((member) => member.name)
+                                .join(
+                                    ", "
+                                )}, GitHub Community SRM team, Technical domain, Corporate domain, Creatives domain, Content domain, GitHub, LinkedIn, Instagram`}
                 />
 
                 <meta
@@ -142,37 +166,93 @@ const Teams = () => {
                 </script>
             </Head>
             <Hero />
+            <div className="text-white">
+                <h2 className="text-2xl sm:text-3xl font-bold text-center mt-16">
+                    Convenor
+                </h2>
+                {convenor && (
+                    <div className="flex justify-center mt-8">
+                        {!fetched ? (
+                            <ProfileSkeleton />
+                        ) : (
+                            <ProfileCard
+                                photo={convenor.pictureUrl}
+                                name={convenor.name}
+                                caption={convenor.caption}
+                                socials={convenor.socials}
+                            />
+                        )
+                        }
+                    </div>
+                )}
+
+                <div className="flex flex-col md:flex-row justify-center items-center gap-8 lg:gap-16 p-8 mt-16">
+                    <div className="flex flex-col items-center">
+                        <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6">
+                            President
+                        </h2>
+                        {president && (
+                            !fetched ? (
+                                <ProfileSkeleton />
+                            ) : (
+                                <ProfileCard
+                                    photo={president.pictureUrl}
+                                    name={president.name}
+                                    caption={president.caption}
+                                    socials={president.socials}
+                                />
+                            )
+                        )}
+                    </div>
+
+                    <div className="flex flex-col items-center">
+                        <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6">
+                            Vice President
+                        </h2>
+                        {vp && (
+                            !fetched ? (
+                                <ProfileSkeleton />
+                            ) : (
+                                <ProfileCard
+                                    photo={vp.pictureUrl}
+                                    name={vp.name}
+                                    caption={vp.caption}
+                                    socials={vp.socials}
+                                />
+                            )
+                        )}
+                    </div>
+                </div>
+            </div>
+
 
             <div className="p-8 md:p-12 lg:px-16 lg:py-24">
                 <div className="flex justify-center items-center relative text-black">
                     <div className="flex flex-wrap justify-center gap-4">
                         <button
                             onClick={() => handleDomainChange("Technical")}
-                            className={`bg-bright_green text-blk font-dmSans h-12 w-32 px-6 rounded-full my-2 md:my-6 shadow-lg transition-transform transform hover:scale-105 ${
-                                domain === "Technical"
-                                    ? " text-black bg-bright_green font-bold"
-                                    : "text-white bg-zinc-900 font-medium"
-                            }`}
+                            className={`bg-bright_green text-blk font-dmSans h-12 w-32 px-6 rounded-full my-2 md:my-6 shadow-lg transition-transform transform hover:scale-105 ${domain === "Technical"
+                                ? " text-black bg-bright_green font-bold"
+                                : "text-white bg-zinc-900 font-medium"
+                                }`}
                         >
                             Technical
                         </button>
                         <button
                             onClick={() => handleDomainChange("Corporate")}
-                            className={`bg-bright_green text-blk font-dmSans h-12 w-32 px-6 rounded-full my-2 md:my-6 shadow-lg transition-transform transform hover:scale-105 ${
-                                domain === "Corporate"
-                                    ? " text-black bg-bright_green font-bold"
-                                    : "text-white bg-zinc-900 font-medium"
-                            }`}
+                            className={`bg-bright_green text-blk font-dmSans h-12 w-32 px-6 rounded-full my-2 md:my-6 shadow-lg transition-transform transform hover:scale-105 ${domain === "Corporate"
+                                ? " text-black bg-bright_green font-bold"
+                                : "text-white bg-zinc-900 font-medium"
+                                }`}
                         >
                             Corporate
                         </button>
                         <button
                             onClick={() => handleDomainChange("Creatives")}
-                            className={` text-blk font-dmSans h-12 w-32 px-6 rounded-full my-2 md:my-6 shadow-lg transition-transform transform hover:scale-105 ${
-                                domain === "Creatives"
-                                    ? " text-black bg-bright_green font-bold"
-                                    : "text-white bg-zinc-900 font-medium"
-                            }`}
+                            className={` text-blk font-dmSans h-12 w-32 px-6 rounded-full my-2 md:my-6 shadow-lg transition-transform transform hover:scale-105 ${domain === "Creatives"
+                                ? " text-black bg-bright_green font-bold"
+                                : "text-white bg-zinc-900 font-medium"
+                                }`}
                         >
                             Creatives
                         </button>
@@ -195,7 +275,7 @@ const Teams = () => {
                         <div>
                             <div className="pt-16 md:p-4 lg:px-4 lg:pt-24">
                                 <h1 className="text-2xl sm:text-4xl font-bold text-center text-white">
-                                    Admins
+                                    Lead
                                 </h1>
                             </div>
                             {!fetched ? (
@@ -208,15 +288,13 @@ const Teams = () => {
                                 </div>
                             ) : (
                                 <div className="flex justify-center flex-wrap gap-8 mt-8 mx-4 md:mx-16">
-                                    {filteredAdmins.map((admin, index) => (
+                                    {filteredLeads.map((leads, index) => (
                                         <ProfileCard
                                             key={index}
-                                            photo={admin.pictureUrl}
-                                            name={admin.name}
-                                            caption={admin.caption}
-                                            linkedin={admin.socials.linkedin}
-                                            instagram={admin.socials.instagram}
-                                            github={admin.socials.github}
+                                            photo={leads.pictureUrl}
+                                            name={leads.name}
+                                            caption={leads.caption}
+                                            socials={leads.socials}
                                         />
                                     ))}
                                 </div>
@@ -224,7 +302,7 @@ const Teams = () => {
 
                             <div className="pt-16 md:p-12 lg:px-16 lg:pt-24">
                                 <h1 className="text-2xl sm:text-4xl font-bold text-center text-white">
-                                    Leads
+                                    Associates
                                 </h1>
                             </div>
                             {!fetched ? (
@@ -237,15 +315,13 @@ const Teams = () => {
                                 </div>
                             ) : (
                                 <div className="flex justify-center flex-wrap gap-8 mt-8 mx-4 md:mx-16">
-                                    {filteredLeads.map((lead, index) => (
+                                    {filteredAssociates.map((associates, index) => (
                                         <ProfileCard
                                             key={index}
-                                            photo={lead.pictureUrl}
-                                            name={lead.name}
-                                            caption={lead.caption}
-                                            linkedin={lead.socials.linkedin}
-                                            instagram={lead.socials.instagram}
-                                            github={lead.socials.github}
+                                            photo={associates.pictureUrl}
+                                            name={associates.name}
+                                            caption={associates.caption}
+                                            socials={associates.socials}
                                         />
                                     ))}
                                 </div>
@@ -272,9 +348,7 @@ const Teams = () => {
                                             photo={member.pictureUrl}
                                             name={member.name}
                                             caption={member.caption}
-                                            linkedin={member.socials.linkedin}
-                                            instagram={member.socials.instagram}
-                                            github={member.socials.github}
+                                            socials={member.socials}
                                         />
                                     ))}
                                 </div>
