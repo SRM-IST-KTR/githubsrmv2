0fbc91a163bec33479904c6ea09e317dbb623694
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-10-20 17:39:56 +0530

    Refactor Sponsors component

--- components/Home/Sponsors.jsx
+++ components/Home/Sponsors.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const sponsors = [
     { name: "SWOC", logo: "https://www.socialwinterofcode.com/img/logos/SWOC_W_Black.png" },
-    { name: "Linux Foundation", logo: "https://www.linuxfoundation.org/hubfs/lf-stacked-color.svg" }
+    // { name: "Linux Foundation", logo: "https://www.linuxfoundation.org/hubfs/lf-stacked-color.svg" }
 ];
 
 const Sponsors = () => {
