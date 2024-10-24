7b783bed5a86208300ec107660ed05bf4dc03801
Utkarsh Jaiswal <1406utkarsh@gmail.com>
2024-10-24 05:54:05 +0530

    upgraded the scripts to use next/scripts

--- pages/team.js
+++ pages/team.js
@@ -4,6 +4,8 @@ import ProfileCard from "@/components/Team/profileCard";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Head from "next/head";
 import { set } from "mongoose";
+import Script from 'next/script';
+
 
 const ProfileSkeleton = () => {
     return (
@@ -115,14 +117,16 @@ const Teams = () => {
         <section className="bg-bg_black">
             <Head>
                 {/* Google Tag Manager */}
-                <script
-                dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-TXXTQDRC');`
-                }}/>
+                <Script
+                    id="google-tag-manager"
+                    dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-TXXTQDRC');`
+                }}
+                />
                 {/* End Google Tag Manager */}
                 <title>Meet the Team | GitHub Community SRM | Technical, Corporate, Creative</title>
                 <meta name="description" content="Meet the GitHub Community SRM team, including President, Vice President, Leads, Associates, and Members from the Technical, Corporate, and Creative domains. Learn more about the talented individuals driving our open-source initiatives." />
